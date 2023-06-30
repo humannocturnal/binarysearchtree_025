78e06d916bd35b63104315e76e60c8df3f77ce4d
humannocturnal <syaputrar374@gmail.com>
2023-06-30 14:54:49 +0700

    obj.insert(word);

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -129,5 +129,21 @@ int main()
 		cout << "3. Perform preorder traversal" << endl;
 		cout << "4. Perform postorder traversal" << endl;
 		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+		}
+		}
 	}
 }
